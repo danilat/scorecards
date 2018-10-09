402b108a5bb36a0e7a6a2fbf63efeb0b66bfc64a
Dani Latorre <dani@danilat.com>
2018-10-09 14:05:29 +0200

    applied google code style

--- src/main/java/com/danilat/scorecards/ScorecardsApplication.java
+++ src/main/java/com/danilat/scorecards/ScorecardsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScorecardsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScorecardsApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ScorecardsApplication.class, args);
+  }
 }
