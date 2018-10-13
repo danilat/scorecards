312081b6501bdaf7e20f59fa6324de766689090b
Dani Latorre <dani@danilat.com>
2018-10-13 19:33:13 +0200

    some minor tests clean up

--- src/test/java/com/danilat/scorecards/core/usecases/RegisterFightTest.java
+++ src/test/java/com/danilat/scorecards/core/usecases/RegisterFightTest.java
@@ -6,17 +6,24 @@ import com.danilat.scorecards.core.domain.Fight;
 import com.danilat.scorecards.core.domain.InvalidFightException;
 import com.danilat.scorecards.core.usecases.RegisterFight.RegisterFightParameters;
 import java.time.LocalDate;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RegisterFightTest {
 
   private static final String ALI = "Ali";
   private static final String FOREMAN = "Foreman";
+  private RegisterFight registerFight;
+  private LocalDate aDate;
+
+  @Before
+  public void setup() {
+    registerFight = new RegisterFight();
+    aDate = LocalDate.now();
+  }
 
   @Test
   public void twoBoxersAndADateTheFightIsRegistered() {
-    RegisterFight registerFight = new RegisterFight();
-    LocalDate aDate = LocalDate.now();
     RegisterFightParameters parameters = new RegisterFightParameters(ALI, FOREMAN, aDate);
 
     Fight fight = registerFight.execute(parameters);
@@ -28,7 +35,6 @@ public class RegisterFightTest {
 
   @Test(expected = InvalidFightException.class)
   public void twoBoxerButNotDateIsInvalid() {
-    RegisterFight registerFight = new RegisterFight();
     RegisterFightParameters parameters = new RegisterFightParameters(ALI, FOREMAN, null);
 
     registerFight.execute(parameters);
@@ -36,8 +42,6 @@ public class RegisterFightTest {
 
   @Test(expected = InvalidFightException.class)
   public void firstBoxerIsNotPresentIsInvalid() {
-    RegisterFight registerFight = new RegisterFight();
-    LocalDate aDate = LocalDate.now();
     RegisterFightParameters parameters = new RegisterFightParameters(null, FOREMAN, aDate);
 
     registerFight.execute(parameters);
@@ -45,8 +49,6 @@ public class RegisterFightTest {
 
   @Test(expected = InvalidFightException.class)
   public void secondBoxerIsNotPresentIsInvalid() {
-    RegisterFight registerFight = new RegisterFight();
-    LocalDate aDate = LocalDate.now();
     RegisterFightParameters parameters = new RegisterFightParameters(ALI, null, aDate);
 
     registerFight.execute(parameters);
