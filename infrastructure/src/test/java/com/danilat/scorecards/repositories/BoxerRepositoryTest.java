47016ac60dead76eb35015bc848dee9577d52480
Dani Latorre <daniellve@inditex.com>
2018-12-16 22:49:39 +0100

    implemented a Boxer repository and the contract test for get and save methods #3

---
+++ infrastructure/src/test/java/com/danilat/scorecards/repositories/BoxerRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.danilat.scorecards.repositories;
+
+import static org.junit.Assert.assertTrue;
+
+import com.danilat.scorecards.core.domain.boxer.Boxer;
+import com.danilat.scorecards.core.domain.boxer.BoxerId;
+import com.danilat.scorecards.core.domain.boxer.BoxerRepository;
+import com.danilat.scorecards.core.mothers.BoxerMother;
+import java.util.Optional;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoxerRepositoryTest {
+
+  private BoxerRepository boxerRepository;
+  private BoxerId boxerId = new BoxerId("some irrelevant id");
+
+  @Before
+  public void setup() {
+    boxerRepository = new InMemoryBoxerRepository();
+  }
+
+  @Test
+  public void saveABoxer() {
+    Boxer aBoxer = BoxerMother.aBoxerWithId(boxerId);
+
+    boxerRepository.save(aBoxer);
+
+    Optional retrieved = boxerRepository.get(boxerId);
+    assertTrue(retrieved.isPresent());
+  }
+
+}
