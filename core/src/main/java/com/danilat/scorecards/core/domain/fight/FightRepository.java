be8635583158cfe3fd38dbd77a4fb7e4586c6175
Dani Latorre <daniellve@inditex.com>
2018-12-17 01:52:24 +0100

    cleanup of FightReporitory methods, are the same than Repository

--- core/src/main/java/com/danilat/scorecards/core/domain/fight/FightRepository.java
+++ core/src/main/java/com/danilat/scorecards/core/domain/fight/FightRepository.java
@@ -1,13 +1,7 @@
 package com.danilat.scorecards.core.domain.fight;
 
 import com.danilat.scorecards.core.domain.Repository;
-import java.util.Optional;
 
 public interface FightRepository extends Repository<Fight, FightId> {
 
-  void save(Fight fight);
-
-  FightId nextId();
-
-  Optional<Fight> get(FightId id);
 }
