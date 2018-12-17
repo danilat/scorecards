5f0cf2af174735d19d6c9da0bbf7bbde450fdece
Dani Latorre <daniellve@inditex.com>
2018-12-17 01:32:39 +0100

    added test for save and get fight repository methods

---
+++ infrastructure/src/test/java/com/danilat/scorecards/repositories/FightRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.danilat.scorecards.repositories;
+
+import static org.junit.Assert.assertTrue;
+
+import com.danilat.scorecards.core.domain.fight.Fight;
+import com.danilat.scorecards.core.domain.fight.FightId;
+import com.danilat.scorecards.core.domain.fight.FightRepository;
+import com.danilat.scorecards.core.mothers.FightMother;
+import java.util.Optional;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FightRepositoryTest {
+
+  private FightRepository fightRepository;
+  private FightId fightId = new FightId("some irrelevant id");
+
+  @Before
+  public void setup() {
+    fightRepository = new InMemoryFightRepository();
+  }
+
+  @Test
+  public void saveABoxer() {
+    Fight aFight = FightMother.aFightWithId(fightId);
+
+    fightRepository.save(aFight);
+
+    Optional retrieved = fightRepository.get(fightId);
+    assertTrue(retrieved.isPresent());
+  }
+}
