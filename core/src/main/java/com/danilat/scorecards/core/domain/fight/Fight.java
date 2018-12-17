cef7c95eb05bc1b1b2159b571cbb8731264d9c5c
Dani Latorre <daniellve@inditex.com>
2018-12-17 01:40:32 +0100

    now Fight domain class extends from Entity

--- core/src/main/java/com/danilat/scorecards/core/domain/fight/Fight.java
+++ core/src/main/java/com/danilat/scorecards/core/domain/fight/Fight.java
@@ -1,16 +1,16 @@
 package com.danilat.scorecards.core.domain.fight;
 
+import com.danilat.scorecards.core.domain.Entity;
 import com.danilat.scorecards.core.domain.boxer.BoxerId;
 
-public class Fight {
+public class Fight extends Entity<FightId> {
 
   private final BoxerId firstBoxer;
   private final BoxerId secondBoxer;
-  private FightId id;
   private Event event;
 
   public Fight(FightId id, BoxerId firstBoxer, BoxerId secondBoxer, Event event) {
-    this.id = id;
+    super(id);
     this.firstBoxer = firstBoxer;
     this.secondBoxer = secondBoxer;
     this.event = event;
