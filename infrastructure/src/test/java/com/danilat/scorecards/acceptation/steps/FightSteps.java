381b7f9c6bd2e259fc83d538bf10caf6511ddae1
Dani Latorre <dani@danilat.com>
2020-02-12 00:14:31 +0100

    Implementing the precoditions to acceptance test for register fight feature happy paths

--- infrastructure/src/test/java/com/danilat/scorecards/acceptation/steps/FightSteps.java
+++ infrastructure/src/test/java/com/danilat/scorecards/acceptation/steps/FightSteps.java
@@ -5,16 +5,23 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 import com.danilat.scorecards.core.domain.ScoreCardsBusinessException;
+import com.danilat.scorecards.core.domain.boxer.Boxer;
+import com.danilat.scorecards.core.domain.boxer.BoxerId;
+import com.danilat.scorecards.core.domain.boxer.BoxerRepository;
 import com.danilat.scorecards.core.domain.fight.Fight;
 import com.danilat.scorecards.core.domain.fight.FightId;
 import com.danilat.scorecards.core.domain.fight.FightRepository;
+import com.danilat.scorecards.core.mothers.BoxerMother;
 import com.danilat.scorecards.core.mothers.FightMother;
 import com.danilat.scorecards.core.usecases.fights.RetrieveAFight;
+import com.danilat.scorecards.repositories.InMemoryBoxerRepository;
 import com.danilat.scorecards.repositories.InMemoryFightRepository;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class FightSteps {
 
@@ -22,10 +29,14 @@ public class FightSteps {
   private Fight retrievedFight;
   private FightRepository fightRepository;
   private Exception someException;
+  private BoxerRepository boxerRepository;
+  private LocalDate aDate;
+  private String aPlace;
 
   @Before
   public void setUp() {
     fightRepository = new InMemoryFightRepository();
+    boxerRepository = new InMemoryBoxerRepository();
   }
 
   @Given("an existing fight")
@@ -62,15 +73,16 @@ public class FightSteps {
   }
 
   @Given("an existing boxer called {string}")
-  public void anExistingBoxerCalled(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new cucumber.api.PendingException();
+  public void anExistingBoxerCalled(String name) {
+    BoxerId boxerId = new BoxerId(name);
+    Boxer aBoxer = BoxerMother.aBoxerWithIdAndName(boxerId, name);
+    boxerRepository.save(aBoxer);
   }
 
   @Given("an event in {string} at {string}")
-  public void anEventInAt(String string, String string2) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new cucumber.api.PendingException();
+  public void anEventInAt(String place, String date) {
+    aDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("d/MM/yyyy"));
+    aPlace = place;
   }
 
   @When("I register the fight in the event for {string} and {string} for {int} rounds")
@@ -87,13 +99,13 @@ public class FightSteps {
   }
 
   @Given("an event at {string}")
-  public void anEventAt(String string) {
+  public void anEventAt(String date) {
     // Write code here that turns the phrase above into concrete actions
     throw new cucumber.api.PendingException();
   }
 
   @Given("an event in {string}")
-  public void anEventIn(String string) {
+  public void anEventIn(String place) {
     // Write code here that turns the phrase above into concrete actions
     throw new cucumber.api.PendingException();
   }
@@ -105,7 +117,7 @@ public class FightSteps {
   }
 
   @When("I register the fight in the event for {string} and {string}")
-  public void iRegisterTheFightInTheEventForAnd(String string, String string2) {
+  public void iRegisterTheFightInTheEventForAnd(String firstBoxer, String secondBoxer) {
     // Write code here that turns the phrase above into concrete actions
     throw new cucumber.api.PendingException();
   }
