9fd0d8df9063fe06cdd0c97acaade7d27ccd76af
Dani Latorre <dani@danilat.com>
2020-08-17 19:35:01 +0200

    Adding the dependecy with spring boot on infrastructure to use the spring annotations in repositories implementations. Related with #12

--- infrastructure/src/main/java/com/danilat/scorecards/repositories/InMemoryBoxerRepository.java
+++ infrastructure/src/main/java/com/danilat/scorecards/repositories/InMemoryBoxerRepository.java
@@ -3,7 +3,9 @@ package com.danilat.scorecards.repositories;
 import com.danilat.scorecards.core.domain.boxer.Boxer;
 import com.danilat.scorecards.core.domain.boxer.BoxerId;
 import com.danilat.scorecards.core.domain.boxer.BoxerRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class InMemoryBoxerRepository extends InMemoryRepository<Boxer, BoxerId> implements BoxerRepository {
 
   @Override
--- infrastructure/src/main/java/com/danilat/scorecards/repositories/InMemoryFightRepository.java
+++ infrastructure/src/main/java/com/danilat/scorecards/repositories/InMemoryFightRepository.java
@@ -6,7 +6,9 @@ import com.danilat.scorecards.core.domain.fight.FightRepository;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class InMemoryFightRepository extends InMemoryRepository<Fight, FightId> implements FightRepository {
 
   @Override
