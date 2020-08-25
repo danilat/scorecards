6c8212000c0491a6a8f9d2c33f5a735a283b6f2f
Dani Latorre <dani@danilat.com>
2020-08-25 12:14:54 +0200

    Renaming AuditUseCaseInitialize to AuditInitializer, it's more proper name about the responsibility

--- infrastructure/src/main/java/com/danilat/scorecards/audit/AuditInitializer.java
+++ infrastructure/src/main/java/com/danilat/scorecards/audit/AuditInitializer.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AuditUseCaseInitializer {
+public class AuditInitializer {
 
   @Autowired
   DomainEventStore domainEventStore;
