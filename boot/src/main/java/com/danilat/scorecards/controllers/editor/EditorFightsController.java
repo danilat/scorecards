ff260cacb3db4e5750f2f7a8490621393d3411c0
Dani Latorre <dani@danilat.com>
2020-08-26 11:24:11 +0200

    Implementing create a new fight action using the usecase #11

--- boot/src/main/java/com/danilat/scorecards/controllers/editor/EditorFightsController.java
+++ boot/src/main/java/com/danilat/scorecards/controllers/editor/EditorFightsController.java
@@ -2,13 +2,21 @@ package com.danilat.scorecards.controllers.editor;
 
 import com.danilat.scorecards.core.domain.boxer.Boxer;
 import com.danilat.scorecards.core.domain.boxer.BoxerId;
+import com.danilat.scorecards.core.domain.fight.Fight;
 import com.danilat.scorecards.core.usecases.boxers.RetrieveAllBoxers;
+import com.danilat.scorecards.core.usecases.fights.RegisterFight;
+import com.danilat.scorecards.core.usecases.fights.RegisterFight.RegisterFightParameters;
+import com.danilat.scorecards.shared.domain.ScoreCardsBusinessException;
+import java.time.LocalDate;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/editor/fights")
@@ -17,10 +25,28 @@ public class EditorFightsController {
   @Autowired
   private RetrieveAllBoxers retrieveAllBoxers;
 
+  @Autowired
+  private RegisterFight registerFight;
+
   @GetMapping("new")
-  public String create(Model model) {
+  public String createForm(Model model) {
     Map<BoxerId, Boxer> boxers = retrieveAllBoxers.execute();
     model.addAttribute("boxers", boxers.values());
     return "editor/fights/new";
+  }
+
+  @PostMapping("")
+  public String create(Model model, @RequestParam String firstBoxer,
+      @RequestParam String secondBoxer,
+      @RequestParam Integer numberOfRounds, @RequestParam String place, @RequestParam
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate happenAt) {
+    try {
+      RegisterFightParameters parameters = new RegisterFightParameters(new BoxerId(firstBoxer),
+          new BoxerId(secondBoxer), happenAt, place, numberOfRounds);
+      Fight fight = registerFight.execute(parameters);
+      return "redirect:/fights/" + fight.id().value();
+    } catch (ScoreCardsBusinessException e) {
+      return createForm(model);
+    }
   }
 }
--- boot/src/test/java/com/danilat/scorecards/controllers/editor/EditorFightsControllerTest.java
+++ boot/src/test/java/com/danilat/scorecards/controllers/editor/EditorFightsControllerTest.java
@@ -2,13 +2,18 @@ package com.danilat.scorecards.controllers.editor;
 
 import static org.hamcrest.Matchers.hasItems;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import com.danilat.scorecards.controllers.BaseControllerTest;
 import com.danilat.scorecards.core.domain.boxer.Boxer;
 import com.danilat.scorecards.core.domain.boxer.BoxerRepository;
 import com.danilat.scorecards.core.mothers.BoxerMother;
+import java.time.LocalDate;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -17,14 +22,46 @@ public class EditorFightsControllerTest extends BaseControllerTest {
   @Autowired
   private BoxerRepository boxerRepository;
 
+  private Boxer firstBoxer;
+  private Boxer secondBoxer;
+
+  @Before
+  public void setup() {
+    firstBoxer = BoxerMother.aBoxerWithId("pacquiao");
+    boxerRepository.save(firstBoxer);
+    secondBoxer = BoxerMother.aBoxerWithId("mayweather");
+    boxerRepository.save(secondBoxer);
+  }
+
   @Test
   public void getsTheFormToCreateAFight() throws Exception {
-    Boxer aBoxer = BoxerMother.aBoxerWithId("pacquiao");
-    boxerRepository.save(aBoxer);
-
     this.mvc.perform(get("/editor/fights/new"))
         .andExpect(status().isOk())
-        .andExpect(model().attribute("boxers", hasItems(aBoxer)));
+        .andExpect(model().attribute("boxers", hasItems(firstBoxer)));
 
+  }
+
+  @Test
+  public void postANewFightWithValidParameters() throws Exception {
+    this.mvc.perform(post("/editor/fights")
+        .param("firstBoxer", firstBoxer.id().value())
+        .param("secondBoxer", secondBoxer.id().value())
+        .param("numberOfRounds", "12")
+        .param("place", "MGM Grand Garden Arena")
+        .param("happenAt", LocalDate.now().toString()))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrlPattern("/fights/{id}"));
+  }
+
+  @Test
+  public void postANewFightWithInValidParameters() throws Exception {
+    this.mvc.perform(post("/editor/fights")
+        .param("firstBoxer", firstBoxer.id().value())
+        .param("secondBoxer",  "foobar")
+        .param("numberOfRounds", "0")
+        .param("place", "MGM Grand Garden Arena")
+        .param("happenAt", LocalDate.now().toString()))
+        .andExpect(status().isOk())
+        .andExpect(view().name("editor/fights/new"));
   }
 }
