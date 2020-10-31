a7c15afe6d6d0e8799371609a211e766a4e6e3f3
Dani Latorre <dani@danilat.com>
2020-10-31 20:34:22 +0100

    Adding an abstraction for usecase exceptions based on input parameters for score and fight validations. Related with #4

--- application/src/main/java/com/danilat/scorecards/core/usecases/fights/InvalidFightException.java
+++ application/src/main/java/com/danilat/scorecards/core/usecases/fights/InvalidFightException.java
@@ -1,37 +1,17 @@
 package com.danilat.scorecards.core.usecases.fights;
 
-import static java.util.stream.Collectors.joining;
-
 import com.danilat.scorecards.core.usecases.fights.RegisterFight.RegisterFightParameters;
-import com.danilat.scorecards.shared.domain.Error;
-import com.danilat.scorecards.shared.domain.Errors;
-import com.danilat.scorecards.shared.domain.ScoreCardsBusinessException;
-import java.util.ArrayList;
-import java.util.List;
+import com.danilat.scorecards.shared.usecases.InvalidParametersException;
+
 import java.util.Set;
 import javax.validation.ConstraintViolation;
 
-public class InvalidFightException extends ScoreCardsBusinessException {
+public class InvalidFightException extends InvalidParametersException {
 
-  private final Errors errors;
-
-  public InvalidFightException(Set<ConstraintViolation<RegisterFightParameters>> violations) {
-    errors = new Errors();
-    violations.stream().forEach(violation -> {
-      Error error = new Error(violation);
-      errors.add(error);
-    });
-  }
-
-  @Override
-  public String getMessage() {
-    List<String> messages = new ArrayList<>();
-    errors.stream()
-        .forEach(error -> messages.add(error.getMessage()));
-    return messages.stream().collect(joining(". "));
-  }
-
-  public Errors getErrors() {
-    return errors;
-  }
+    public InvalidFightException(Set<ConstraintViolation<RegisterFightParameters>> violations) {
+        super();
+        violations.stream().forEach(violation -> {
+            addParameterViolation(violation);
+        });
+    }
 }
