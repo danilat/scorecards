17f7c9d0edb738454e55f9512b3dfdf28201a070
Dani Latorre <dani@danilat.com>
2021-01-01 20:27:53 +0100

    Adding the controller action and view to create new acconunts #29

--- boot/src/main/java/com/danilat/scorecards/controllers/AccountController.java
+++ boot/src/main/java/com/danilat/scorecards/controllers/AccountController.java
@@ -1,13 +1,20 @@
 package com.danilat.scorecards.controllers;
 
+import com.danilat.scorecards.core.domain.account.Account;
+import com.danilat.scorecards.core.usecases.accounts.RegisterAccount;
+import com.danilat.scorecards.core.usecases.accounts.RegisterAccount.RegisterAccountParameters;
+import com.danilat.scorecards.shared.PrimaryPort;
 import com.danilat.scorecards.shared.auth.firebase.TokenValidator;
 import com.danilat.scorecards.shared.auth.firebase.TokenValidator.Token;
+import com.danilat.scorecards.shared.domain.FieldErrors;
 import com.google.firebase.auth.FirebaseAuthException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,13 +24,75 @@ public class AccountController {
   @Autowired
   private TokenValidator tokenValidator;
 
+  @Autowired
+  private RegisterAccount registerAccount;
+
   @GetMapping("new/{idToken}")
-  public String createForm(@PathVariable String idToken, Model model) throws FirebaseAuthException {
-    Token token = tokenValidator.validateToken(idToken);
-    model.addAttribute("name", token.getName());
-    model.addAttribute("email", token.getEmail());
-    model.addAttribute("picture", token.getPicture());
+  public String createForm(@PathVariable String idToken, Model model) {
+    try {
+      Token token = tokenValidator.validateToken(idToken);
+      model.addAttribute("idToken", idToken);
+      model.addAttribute("name", token.getName());
+      model.addAttribute("email", token.getEmail());
+      model.addAttribute("picture", token.getPicture());
+    } catch (FirebaseAuthException e) {
+      throw new RuntimeException(e);
+    }
     return "accounts/new";
   }
 
+  private Model model;
+  private String createResult;
+  private String idToken;
+
+  private PrimaryPort<Account> registerAccountPrimaryPort = new PrimaryPort<Account>() {
+
+    @Override
+    public void success(Account account) {
+      createResult = "redirect:/";
+    }
+
+    @Override
+    public void error(FieldErrors errors) {
+      model.addAttribute("errors", errors);
+      createResult = createForm(idToken, model);
+    }
+  };
+
+  @PostMapping("{idToken}")
+  public String create(@PathVariable String idToken, @ModelAttribute AccountForm accountForm, Model model)
+      throws FirebaseAuthException {
+    this.idToken = idToken;
+    this.model = model;
+    Token token = tokenValidator.validateToken(idToken);
+    RegisterAccountParameters params = new RegisterAccountParameters(accountForm.getUsername(), accountForm.getName(),
+        token.getEmail(), token.getPicture());
+    registerAccount.execute(registerAccountPrimaryPort, params);
+    return createResult;
+  }
+
+  private class AccountForm {
+
+    private String username;
+    private String name;
+
+    public AccountForm() {
+    }
+
+    public String getUsername() {
+      return username;
+    }
+
+    public void setUsername(String username) {
+      this.username = username;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+  }
 }
--- boot/src/test/java/com/danilat/scorecards/controllers/AccountControllerTest.java
+++ boot/src/test/java/com/danilat/scorecards/controllers/AccountControllerTest.java
@@ -2,23 +2,61 @@ package com.danilat.scorecards.controllers;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import com.danilat.scorecards.shared.auth.firebase.TokenValidator;
+import com.danilat.scorecards.core.domain.account.AccountRepository;
 import com.danilat.scorecards.shared.auth.firebase.TokenValidator.Token;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class AccountControllerTest extends BaseControllerTest {
+
+  @Autowired
+  private AccountRepository accountRepository;
+
+  @Before
+  public void setUp() {
+    accountRepository.clear();
+  }
 
   @Test
   public void newAccountForm() throws Exception {
     String theToken = "some_valid_token";
-    Token token = new Token("a name", "an email", "a picture");
+    Token token = new Token("Dani", "dani@email.com", "a picture url");
     when(tokenValidator.validateToken(theToken)).thenReturn(token);
 
     this.mvc.perform(
         get("/accounts/new/" + theToken))
         .andExpect(status().isOk());
+  }
+
+  @Test
+  public void postANewAccountWithValidParameters() throws Exception {
+    String theToken = "some_valid_token";
+    Token token = new Token("Dani", "dani@email.com", "a picture url");
+    when(tokenValidator.validateToken(theToken)).thenReturn(token);
+
+    this.mvc.perform(post("/accounts/" + theToken)
+        .param("name", "Dani")
+        .param("username", "danilat"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrl("/"));
+  }
+
+  @Test
+  public void postANewAccountWithInValidParameters() throws Exception {
+    String theToken = "some_valid_token";
+    Token token = new Token("Dani", "dani@email.com", "a picture url");
+    when(tokenValidator.validateToken(theToken)).thenReturn(token);
+
+    this.mvc.perform(post("/accounts/" + theToken)
+        .param("name", "Dani")
+        .param("username", ""))
+        .andExpect(status().isOk())
+        .andExpect(view().name("accounts/new"));
   }
 }
