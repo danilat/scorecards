617894471ecad3324a2b2f6e6838533212fcb7cd
Dani Latorre <dani@danilat.com>
2021-01-31 20:43:42 +0100

    Removing the repository population on app on loading, was a workaround for temporal in memory repositories #35

--- boot/src/main/java/com/danilat/scorecards/ScorecardsApplication.java
+++ boot/src/main/java/com/danilat/scorecards/ScorecardsApplication.java
@@ -1,44 +1,12 @@
 package com.danilat.scorecards;
 
-import com.danilat.scorecards.core.domain.boxer.Boxer;
-import com.danilat.scorecards.core.domain.boxer.BoxerId;
-import com.danilat.scorecards.core.domain.boxer.BoxerRepository;
-import com.danilat.scorecards.core.domain.fight.Event;
-import com.danilat.scorecards.core.domain.fight.Fight;
-import com.danilat.scorecards.core.domain.fight.FightId;
-import com.danilat.scorecards.core.domain.fight.FightRepository;
-import java.time.LocalDate;
-import javax.annotation.PostConstruct;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScorecardsApplication {
 
-  private static final Logger log = LoggerFactory.getLogger(ScorecardsApplication.class);
-
   public static void main(String[] args) {
     SpringApplication.run(ScorecardsApplication.class, args);
-  }
-
-  @Autowired
-  FightRepository fightRepository;
-
-  @Autowired
-  BoxerRepository boxerRepository;
-
-  @PostConstruct
-  public void init() {
-    log.info("ScorecardsApplication initialization logic ...");
-    Boxer ali = new Boxer(new BoxerId("ali"), "Muhammad Ali");
-    boxerRepository.save(ali);
-    Boxer foreman = new Boxer(new BoxerId("foreman"), "George Foreman");
-    boxerRepository.save(foreman);
-    Event event = new Event(LocalDate.now(), "Kinasa, Zaire");
-    Fight fight = new Fight(new FightId("1"), ali.id(), foreman.id(), event, 12);
-    fightRepository.save(fight);
   }
 }
