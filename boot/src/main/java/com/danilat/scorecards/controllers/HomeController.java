5fdcfbacbc737fd9045eb42594ce457a822a71e4
Dani Latorre <dani@danilat.com>
2021-02-14 14:40:39 +0100

    Adding the list of the last past fights in the home page. Closes #37

--- boot/src/main/java/com/danilat/scorecards/controllers/HomeController.java
+++ boot/src/main/java/com/danilat/scorecards/controllers/HomeController.java
@@ -1,13 +1,29 @@
 package com.danilat.scorecards.controllers;
 
+import com.danilat.scorecards.core.domain.fight.projections.FightWithBoxers;
+import com.danilat.scorecards.core.usecases.fights.RetrieveLastPastFights;
+import com.danilat.scorecards.shared.PrimaryPort;
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+  @Autowired
+  private RetrieveLastPastFights retrieveLastPastFights;
+  Collection<FightWithBoxers> lastFights;
+
+  private PrimaryPort<Collection<FightWithBoxers>> retrieveLastPastFightsPort() {
+    return fights -> lastFights = fights;
+  }
+
   @GetMapping("/")
-  public String index() {
+  public String index(Model model) {
+    retrieveLastPastFights.execute(retrieveLastPastFightsPort());
+    model.addAttribute("lastFights", lastFights);
     return "home";
   }
 }
--- boot/src/test/java/com/danilat/scorecards/controllers/HomeControllerTest.java
+++ boot/src/test/java/com/danilat/scorecards/controllers/HomeControllerTest.java
@@ -1,15 +1,46 @@
 package com.danilat.scorecards.controllers;
 
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.danilat.scorecards.core.domain.boxer.BoxerRepository;
+import com.danilat.scorecards.core.domain.fight.Fight;
+import com.danilat.scorecards.core.domain.fight.FightRepository;
+import com.danilat.scorecards.core.mothers.BoxerMother;
+import com.danilat.scorecards.core.mothers.FightMother;
+import java.time.LocalDate;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class HomeControllerTest extends BaseControllerTest {
+
+  @Autowired
+  private FightRepository fightRepository;
+  @Autowired
+  private BoxerRepository boxerRepository;
 
   @Test
   public void rootPathIsAvailable() throws Exception {
     this.mvc.perform(get("/"))
         .andExpect(status().isOk());
+  }
+
+  @Test
+  public void hasTheLastFightsThatHappened() throws Exception {
+    boxerRepository.clear();
+    fightRepository.clear();
+    Fight fight = FightMother.aFightWithHappenAt(LocalDate.now().minusDays(1));
+    boxerRepository.save(BoxerMother.aBoxerWithId(fight.firstBoxer()));
+    boxerRepository.save(BoxerMother.aBoxerWithId(fight.secondBoxer()));
+    fightRepository.save(fight);
+
+    this.mvc.perform(get("/"))
+        .andExpect(status().isOk())
+        .andExpect(model().attribute("lastFights", notNullValue()))
+        .andExpect(model().attribute("lastFights", hasSize(1)));
   }
 }
